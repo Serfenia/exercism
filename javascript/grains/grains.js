8f1b2defde93263f32d85bc1f0ae8af0a09b599c
Patrick van Vuuren <patrick@proforto.nl>
2016-11-17 09:26:09 +0100

    Updated javascript:grains by using pow instead of recursion for total

--- javascript/grains/grains.js
+++ javascript/grains/grains.js
@@ -2,19 +2,10 @@ const BigInt = require('./big-integer');
 
 module.exports = class Grains {
     square(to) {
-        return this.getValue(to);
-    }
-
-    getValue(to) {
         return to === 1 ? '1' : (new BigInt(2)).pow(to - 1).toString();
     }
 
-    sumRange(from, to, int) {
-        int = int.add(this.getValue(from));
-        return from < to ? this.sumRange(from + 1, to, int) : int;
-    }
-
     total() {
-        return this.sumRange(1, 64, new BigInt(0)).toString();
+        return (new BigInt(2)).pow(64).minus(1).toString();
     }
 };
