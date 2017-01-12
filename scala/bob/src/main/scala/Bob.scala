1115a284e5d2d3e218ebd65ca725ce8ff4fc9f81
Patrick van Vuuren <patrick@proforto.nl>
2017-01-12 22:16:50 +0100

    Improved scala: bob

--- scala/bob/src/main/scala/Bob.scala
+++ scala/bob/src/main/scala/Bob.scala
@@ -4,9 +4,9 @@ class Bob {
   def hey(str: String) : String = {
     val pattern = "[A-Za-z]".r;
     Element(str.trim(), str.toUpperCase() == str && pattern.findFirstIn(str) != None, str.takeRight(1)) match {
-      case Element("", _, _) => return "Fine. Be that way!";
-      case Element(_, true, _) => return "Whoa, chill out!";
-      case Element(_, _, "?") => return "Sure.";
+      case Element("", _, _) =>  "Fine. Be that way!"
+      case Element(_, true, _) =>  "Whoa, chill out!"
+      case Element(_, _, "?") => "Sure."
       case _ => "Whatever."
     }
   }
